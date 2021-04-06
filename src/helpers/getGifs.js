f224b62eec30da42876afa2622ae50d559a33c13
dquinoneslopez <dquinoneslopez@gmail.com>
2021-04-06 11:27:14 +0200

    Minor change.

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,12 +1,12 @@
-export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=15&api_key=${process.env.API_KEY}`;
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=15&api_key=${process.env.API_KEY}`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(img => {
         return {
             id: img.id,
             title: img.title,
-            url: img.images ? .downsized_medium.url
+            url: img.images?.downsized_medium.url
         }
     });
 
